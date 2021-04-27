b347d6684a1b704d6d5a570835d8e9780bb7554d
DraftTin <474827656@qq.com>
2021-04-27 11:05:41 +0800

    去掉数据库配置

--- blog-backend/settings.py
+++ blog-backend/settings.py
@@ -93,25 +93,6 @@ WSGI_APPLICATION = 'blog-backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.oracle',
-#         'NAME': '127.0.0.1:1521/TestDB',
-#         'USER': 'SYSTEM',
-#         'PASSWORD': '654321'
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'postgres',
-        'PASSWORD': 'Xgt656728474!?',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
